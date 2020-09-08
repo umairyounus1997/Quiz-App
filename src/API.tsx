1aac80993c9eeccbc9207f1178383ce0457d3df6
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-09-08 21:52:07 +0500

    Initial App

---
+++ src/API.tsx
@@ -0,0 +1,23 @@
+export const fetchQuestions = async (amount: number, difficulty : Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await(await fetch(endpoint)).json();
+    console.log(data);
+
+}
+
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export  type QuestionState = Question & { answers: string[] };
---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import { QuestionCard } from './components/QuestionCard';
+import {fetchQuestions, Difficulty} from './API';
+
+const TOTAL_QUESTIONS = 10;
+
+function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  
+
+  const startQuiz = async() => {};
+
+  const nextQuestion = async() => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+return (
+    <div className="App">
+      <h1>Quiz</h1>
+      <button className='start' onClick={startQuiz}> 
+        Begin Quiz 
+      </button>
+      <p className='score'> Score: </p>
+      <p className='loading'> Loading... </p>
+      {/* <QuestionCard 
+        questionNum = {number + 1}
+        totalQuestions = {TOTAL_QUESTIONS}
+        question = {questions[number].question}
+        answers = {questions[number].answers}
+        userAnswer = {userAnswers ? userAnswers[number] : undefined }
+        callback = {checkAnswer}
+        /> */}
+      <button className='next' onClick={nextQuestion}> Next </button>
+    </div>
+  );
+}
+
+export default App;
