34efefd9f18159d853612f698c530a3de760e53d
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-09-11 22:38:23 +0500

    11Sep20

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,24 @@ function App() {
 
   const nextQuestion = async() => {};
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver){
+      const answer = e.currentTarget.value;
+
+      const correct = questions[number].correct_answer === answer;
+
+      if (correct) setScore(prev => prev + 1)
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+
+      setUserAnswers(prev => [...prev, answerObject])
+    }
+  };
 
 return (
     <div className="App">
