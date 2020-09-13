5c5a13ac6f8a16ca18ae5d4361fc1d1c3544e470
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2020-09-13 15:43:32 +0500

    Update QuestionCard.tsx

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -19,10 +19,13 @@ export const QuestionCard: React.FC<Props> = ({question, answers, callback, user
     <p dangerouslySetInnerHTML={{ __html: question}} />
     <div>
       {answers.map(answer =>(
-        <ButtonWrapper>
+        <ButtonWrapper
+          correct = {userAnswer?.correctAnswer === answer}
+          userClicked = {userAnswer?.answer === answer }
+
+        >
           <button disabled={userAnswer} value={answer} onClick={callback}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
-
           </button>
         </ButtonWrapper>
       ))}
